f3de3c6ed0889104dd677c3c7285bc417e188cf7
mohammad-rezaei77 <mrezaei073@gmail.com>
2022-09-20 14:25:36 +0430

    created render html files
    added templates html files

--- website/views.py
+++ website/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse('<h1>HOME PAGE</h1>')
+    return render(request,"website/index.html")
 
 
 def blog_view(request):
-    return HttpResponse('<h1>blog PAGE</h1>')
+    return render(request,"blog.html")
 
 
 def contact_view(request):
-    return HttpResponse('<h1>contact PAGE</h1>')
+    return render(request,"contact.html")

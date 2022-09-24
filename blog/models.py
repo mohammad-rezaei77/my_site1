0dd7ad0e5b55fc3def9734ce950ff76f574e7d3b
mohammad-rezaei77 <mrezaei073@gmail.com>
2022-09-24 18:52:46 +0330

    create admin orm and init database
    create table in blog and website with class on models.py
    class blog_post and website_contact

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+
 from django.db import models
 
 # Create your models here.
+class post2(models.Model):
+    title=models.CharField(max_length=255)
+    name=models.CharField(max_length=255)
+    content=models.TextField()
+    counted_view=models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    publish_date=models.DateField(null=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    update_date=models.DateTimeField(auto_now_add=True)
+    #tag image author categorys

9e0ff0fe28ceab80fe2f235585b525ee51be07e8
mohammad-rezaei77 <mrezaei073@gmail.com>
2022-09-26 13:53:00 +0330

    static pages
    pages : blog-home and blog-single
    static and connect database

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,16 @@
 
 from operator import index
-from django.urls import path
+from xml.etree.ElementInclude import include
+from django.urls import path ,include
 from blog.views import *
 app_name='blog'
 urlpatterns = [
-
+    #path('',include('website.urls')),
     path('',blog_view,name='blog'),
-    path('blog/single',single_view,name='single'),
+
+    path('<int:pid>',single_view,name='single'),
+
+    path('test',test,name='test')
 
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
 
+from datetime import datetime
+from django.shortcuts import render,get_object_or_404
+from blog.models import post2
+t=datetime.now()
 def blog_view(request):
-    return render(request,"blog/blog-home.html")
+
+    
+    posts=post2.objects.filter(status=1,publish_date__lte=t)
+    context={'posts': posts}
+    #pub_date={'publish_date':context.publish_date}
+    
+    return render(request,"blog/blog-home.html",context)
 
 
-def single_view(request):
-    return render(request,"blog/blog-single.html")
+def single_view(request,pid):
+    
+    post=get_object_or_404(post2,id=pid,status=1,publish_date__lte=t)
+    
+    post.counted_view+=1       #added counted_view
+    post.save()
+   # post=post2.objects.get(id=pid)
+    context={'post':post}
+    return render(request,"blog/blog-single.html",context)
 
+
+def test(request):
+    posts=post2.objects.filter(publish_date__lte=t)
+    context={'posts': posts}
+    return render(request,"blog/test.html",context)
